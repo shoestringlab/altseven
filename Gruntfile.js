32f0db2d1b633aa5c12b399cf0d46b3a39d653cd
Robert Munn <robert.d.munn@gmail.com>
2017-01-15 20:11:11 -0700

    - updated grunt - added git ignore

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+
+module.exports = function(grunt) {
+	var path = require('path');
+
+	require('load-grunt-config')(grunt, {
+	    configPath: path.join(process.cwd(), 'grunt'), 
+	    init: true, 
+	    data: { 
+	    	 pkg: require('./package.json')
+	    },
+	    loadGruntTasks: { 
+	    	pattern: ['grunt-contrib-*', 'grunt-jslint', 'grunt-newer'],
+	        scope: 'devDependencies'
+	    },
+	    postProcess: function(config) {} 
+	});
+	grunt.registerTask("default", ["newer:concat:objects", "newer:jslint:src", "newer:concat:css", "newer:concat:a7", "newer:jslint:dist", "newer:uglify"]);
+	grunt.registerTask("clean", ["concat:objects", "jslint:src", "concat:css", "concat:a7", "jslint:dist", "uglify"]);
+	grunt.registerTask("lint", ["newer:concat:objects", "newer:jslint"]);
+};
