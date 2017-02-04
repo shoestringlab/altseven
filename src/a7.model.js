3898efa27af43a641e9b8e6204786fa073a432f4
Robert Munn <robert.d.munn@gmail.com>
2017-02-04 13:59:24 -0700

    - convert model to pluggable module provider so users can employ alternate models

--- src/a7.model.js
+++ src/a7.model.js
@@ -1,1 +1,42 @@
-a7.Model = gadgetui.model;
+a7.Model = ( function() {
+	"use strict";
+	var _model,
+		_methods = {};
+
+	return {
+		create : function(){
+			return _methods[ "create" ].apply( _model, arguments );
+		},
+		destroy : function(){
+			return _methods[ "destroy" ].apply( _model, arguments );
+		},
+		get : function(){
+			return _methods[ "get" ].apply( _model, arguments );
+		},
+		set : function(){
+			return _methods[ "set" ].apply( _model, arguments );
+		},
+		exists : function(){
+			return _methods[ "exists" ].apply( _model, arguments );
+		},
+		bind : function(){
+			return _methods[ "bind" ].apply( _model, arguments );
+		},
+		init: function( options, resolve, reject ){
+			a7.Log.info( "Model initializing... " );
+			switch( options.model ){
+				case "gadgetui":
+					_model = gadgetui.model;
+					// gadgetui maps directly, so we can loop on the keys
+					Object.keys( gadgetui.model ).forEach( function( key, index ){
+						if( key !== "BindableObject" ){
+							_methods[ key ] = gadgetui.model[ key ];
+						}
+					});
+					break;
+			}
+			resolve();
+		}
+	};
+
+}() );
