d978b33b74a28321ecb4a12b5d9b60d1fef16995
Robert Munn <robert.d.munn@gmail.com>
2017-02-04 13:56:09 -0700

    - get options from model

--- src/a7.ui.js
+++ src/a7.ui.js
@@ -1,64 +1,74 @@
-a7.Layout = ( function() {
+a7.UI = ( function() {
 		"use strict";
 
-		var _selectors = {},
+		var _options = {},
+			_selectors = {},
 			_templateMap = {},
 
-		_setSelector = function( name, selector ){
-			_selectors[ name ] = selector;
-		},
-
-		_addTemplate = function( key, html ){
-			_templateMap[ key ] = html.trim();
-		},
-
-		_loadTemplates = function( templates, resolve, reject ){
-			var ot = Math.ceil( Math.random( ) * 500 );
-
-			switch( a7.Model.get( "renderer" ) ){
-				case "mustache":
-					fetch( templates + '?' + ot )
-						.then( function( response ) {
-							return response.text();
-						})
-						.then( function( text ){
-							a7.Log.info( "Loading Mustache templates... " );
-							var parser = new DOMParser(),
-								doc = parser.parseFromString( text, "text/html" ),
-								scripts = doc.querySelectorAll( "script" );
-							scripts.forEach( function( script ){
-								_addTemplate( script.getAttribute( "id" ), script.innerHTML );
+			_setSelector = function( name, selector ){
+				_selectors[ name ] = selector;
+			},
+	
+			_addTemplate = function( key, html ){
+				switch( _options.renderer ){
+					case "Mustache":
+						_templateMap[ key ] = html.trim();
+						break;
+					case "Handlebars":
+						_templateMap[ key ] = Handlebars.compile( html.trim() );
+						break;
+				}
+			},
+	
+			_loadTemplates = function( resolve, reject ){
+				var ot = Math.ceil( Math.random( ) * 500 );
+	
+				switch( _options.renderer ){
+					case "Mustache":
+					case "Handlebars":
+						fetch( _options.templates + '?' + ot )
+							.then( function( response ) {
+								return response.text();
+							})
+							.then( function( text ){
+								a7.Log.info( "Loading " + _options.renderer + " templates... " );
+								var parser = new DOMParser(),
+									doc = parser.parseFromString( text, "text/html" ),
+									scripts = doc.querySelectorAll( "script" );
+								scripts.forEach( function( script ){
+									_addTemplate( script.getAttribute( "id" ), script.innerHTML );
+								});
+								resolve();
 							});
-							resolve();
-						});
-
+	
+						break;
+				}
+			},
+			
+			_render = function( template, params ){
+				switch( _options.renderer ){
+				case "Mustache":
+					return Mustache.to_html( _templateMap[ template ], params, _templateMap );
 					break;
-				case "handlebars":
-					//not implemented
-					resolve();
+				case "Handlebars":
+					return _templateMap[ template ]( params );
 					break;
-			}
-		},
-		
-		_render = function( template, params ){
-			switch( a7.Model.get( "renderer" ) ){
-			case "mustache":
-				return Mustache.to_html( _templateMap[ template ], params, _templateMap );
-				break;
-			}
-		};
+				}
+			};
 
 		return{
 			render : _render,
 			selectors: _selectors,
 			setSelector: _setSelector,
-			init : function( options, resolve, reject ){
-				var renderers = "handlebars,mustache";
+			init : function( resolve, reject ){
+				var renderers = "Handlebars,Mustache";
+				_options = a7.Model.get( "a7.UI" );
+				
 				a7.Log.info( "Layout initializing..." );
-				if( renderers.indexOf( a7.Model.get( "renderer" ) ) >=0 ){
-					a7.Model.set( "templatesLoaded", false );
-					if( options.templates !== undefined ){
-						_loadTemplates( options.templates, resolve, reject );
+				if( renderers.indexOf( _options.renderer ) >=0 ){
+					a7.Model.set( "a7.UI.templatesLoaded", false );
+					if( _options.templates !== undefined ){
+						_loadTemplates( resolve, reject );
 					}
 				}else{
 					resolve();
