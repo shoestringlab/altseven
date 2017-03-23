f87247d81e9d7131e5762ca18bda26f91c5764ef
Robert Munn <robert.d.munn@gmail.com>
2017-03-23 15:11:45 -0600

    - add error module to a7 for global JS error handling

---
+++ src/a7.error.js
@@ -0,0 +1,34 @@
+a7.error = ( function(){
+  "use strict";
+
+  var _captureError = function( msg, url, lineNo, columnNo, error ){
+    var string = msg.toLowerCase();
+    var substring = "script error";
+    if (string.indexOf(substring) > -1){
+        a7.log.error('Script Error: See Browser Console for Detail');
+    } else {
+        var message = [
+            'Message: ' + msg,
+            'URL: ' + url,
+            'Line: ' + lineNo,
+            'Column: ' + columnNo,
+            'Error object: ' + JSON.stringify(error)
+        ].join(' - ');
+
+        a7.log.error( message );
+    }
+  };
+
+  window.onerror = function (msg, url, lineNo, columnNo, error) {
+    a7.error.captureError( msg, url, lineNo, columnNo, error );
+    return false;
+  };
+
+  return {
+    capture: function( error ){
+
+
+    },
+    captureError : _captureError
+  }
+}());
