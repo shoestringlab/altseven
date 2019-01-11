5c7d3a401bf488d5c24d2d2c809fcda18ebde371
Robert Munn <robert.d.munn@gmail.com>
2019-01-11 16:01:24 -0700

    - no websocket url required, will not config unless provided
    - The console will no longer try to configure a Web Socket listener unless the websocket url is provided in the options.

--- src/a7.console.js
+++ src/a7.console.js
@@ -1,3 +1,4 @@
+
 a7.console = (function() {
   "use strict";
 
@@ -29,7 +30,7 @@ a7.console = (function() {
     init: function(resolve, reject) {
       var console = a7.model.get("a7.console");
       if( console.container === "" ) reject( "You must specify a container object for the console display." );
-      
+
       // check for console state
       if ( console.enabled ) {
         active = true;
@@ -37,7 +38,8 @@ a7.console = (function() {
         consoleDiv.setAttribute("id", "consoleDiv");
         consoleDiv.setAttribute("class", "a7-console");
         document.body.append(consoleDiv);
-      var connection,
+
+        var connection,
           fp = a7.components.Constructor(
             console.container,
             [
@@ -57,79 +59,79 @@ a7.console = (function() {
 
         fp.selector.setAttribute("right", 0);
 
-        window.WebSocket = window.WebSocket || window.MozWebSocket;
+        if( console.wsServer ){
+          window.WebSocket = window.WebSocket || window.MozWebSocket;
 
-        // if browser doesn't support WebSocket, just show some
-        // notification and exit
-        if (!window.WebSocket) {
-          consoleDiv.innerHTML("Your browser doesn't support WebSockets.");
-          return;
-        }
-
-        // open connection
-        connection = new WebSocket(console.wsServer);
-
-        connection.onopen = function() {
-          //a7.log.info( "Console initializing..." );
-        };
-
-        connection.onerror = function() {
-          var message = "Can't connect to the console socket server.";
-          if (console.enabled) {
-            // just in there were some problems with conenction...
-            _addMessage(message, new Date(), "local");
-          } else {
-            a7.log.error(message);
-          }
-        };
-
-        // most important part - incoming messages
-        connection.onmessage = function(message) {
-          var json, ix;
-          // try to parse JSON message. Because we know that the
-          // server always returns
-          // JSON this should work without any problem but we should
-          // make sure that
-          // the massage is not chunked or otherwise damaged.
-          try {
-            json = JSON.parse(message.data);
-          } catch (er) {
-            a7.log.error("This doesn't look like valid JSON: ", message.data);
+          // if browser doesn't support WebSocket, just show some
+          // notification and exit
+          if (!window.WebSocket) {
+            consoleDiv.innerHTML("Your browser doesn't support WebSockets.");
             return;
           }
 
-          if (json.type === "history") {
-            // entire message
-            // history
-            // insert every single message to the chat window
-            for (ix = 0; ix < json.data.length; ix++) {
-              _addMessage(
-                json.data[ix].text,
-                new Date(json.data[ix].time),
-                "websocket"
-              );
-            }
-          } else if (json.type === "message") {
-            // it's a single
-            // message
-            _addMessage(json.data.text, new Date(json.data.time), "websocket");
-          } else {
-            a7.log.error("This doesn't look like valid JSON: ", json);
-          }
-        };
+          // open connection
+          connection = new WebSocket(console.wsServer);
 
-        window.addEventListener("close", function() {
-          connection.close();
-        });
+          connection.onopen = function() {
+            //a7.log.info( "Console initializing..." );
+          };
+
+          connection.onerror = function() {
+            var message = "Can't connect to the console socket server.";
+            if (console.enabled) {
+              // just in there were some problems with conenction...
+              _addMessage(message, new Date(), "local");
+            } else {
+              a7.log.error(message);
+            }
+          };
+
+          // most important part - incoming messages
+          connection.onmessage = function(message) {
+            var json, ix;
+            // try to parse JSON message. Because we know that the
+            // server always returns
+            // JSON this should work without any problem but we should
+            // make sure that
+            // the massage is not chunked or otherwise damaged.
+            try {
+              json = JSON.parse(message.data);
+            } catch (er) {
+              a7.log.error("This doesn't look like valid JSON: ", message.data);
+              return;
+            }
+
+            if (json.type === "history") {
+              // entire message
+              // history
+              // insert every single message to the chat window
+              for (ix = 0; ix < json.data.length; ix++) {
+                _addMessage(
+                  json.data[ix].text,
+                  new Date(json.data[ix].time),
+                  "websocket"
+                );
+              }
+            } else if (json.type === "message") {
+              // it's a single
+              // message
+              _addMessage(json.data.text, new Date(json.data.time), "websocket");
+            } else {
+              a7.log.error("This doesn't look like valid JSON: ", json);
+            }
+          };
+
+          window.addEventListener("close", function() {
+            connection.close();
+          });
+        }
 
         a7.console.addMessage = _addMessage;
         a7.log.info("Console initializing...");
         resolve();
       } else {
         // console init should not run if console is set to false
-        reject(
-          "Console init should not be called when console option is set to false."
-        );
+        reject( "Console init should not be called when console option is set to false." );
       }
     }
   };
